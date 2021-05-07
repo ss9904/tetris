12daa9a08a8cdc5e550ba1f19e41e506a995fc5b
ss9904 <yatsui.shunki.sj@alumni.tsukuba.ac.jp>
2021-05-07 22:29:37 +0900

    05/07/2021/zzz

---
+++ UI.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from tkinter.constants import NONE
+
+
+# constants
+BLOCK_SIZE = 25
+FIELD_WIDTH = 10
+FIELD_HEIGHT = 24
+
+
+class Window(tk.Frame):
+    """
+    ゲームウィンドウのクラス
+    """
+
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        
+        self.master.geometry("600x800")
+        self.master.title("test")
+
+
+    def make_game_field(self):
+        field = GameField()
+        field.place(x=25, y=50)
+
+
+
+class GameField(tk.Canvas):
+    """テトリミノが積みあがる画面"""
+
+    def __init__(self):
+
+        canvas_width = BLOCK_SIZE * FIELD_WIDTH
+        canvas_height = BLOCK_SIZE * FIELD_HEIGHT
+
+        tk.Canvas.__init__(
+            self, master=None,
+            width=canvas_width, height=canvas_height,
+            )
+
+        for y in range(FIELD_HEIGHT):
+            for x in range(FIELD_WIDTH):
+                x0 = x*BLOCK_SIZE
+                y0 = y*BLOCK_SIZE
+                x1 = (x+1)*BLOCK_SIZE
+                y1 = (y+1)*BLOCK_SIZE
+
+                self.create_rectangle(
+                    x0, y0, x1, y1, fill="gray70",
+                    outline="white", width=1
+                )
+
+
+
+if __name__ == "__main__":
+    gameWindow = Window()
+    gameWindow.make_game_field()
+    gameWindow.mainloop()
--- tkinterTest.py
+++ tkinterTest.py
@@ -1,26 +1,26 @@
-from hoge import main
-import tkinter
+import tkinter as tk
 
-class Application(tkinter.Frame):
+class Application(tk.Frame):
     def __init__(self, master=None):
-        super().__init__(master)
+        tk.Frame.__init__(self, master)
 
-        self.master.geometry("400x600")
+        self.master.geometry("600x800")
         self.master.title("test")
 
+        a = tk.Canvas(width=550, height=700, bg="cyan")
 
-        toolbar = tkinter.Frame(self.master)
-        
-        for i in range(3):
-            button= tkinter.Button(toolbar, text=str(i), width=2)
-            button.pack(side=tkinter.LEFT)
+        for y in range(10):
+            for x in range(5):
+                a.create_rectangle(
+                    x*25, y*25, (x+1)*25, (y+1)*25,
+                    outline="white", width=1,
+                    fill="gray"
+                )
 
-        toolbar.pack(fill=tkinter.X)
-
-
+        a.place(x=25, y=50)
 
 
 if __name__ == "__main__":
-    root = tkinter.Tk()
+    root = tk.Tk()
     app = Application(master=root)
     app.mainloop()
